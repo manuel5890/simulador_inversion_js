6f754ffa45c67ed2ccd37af0f62d17828cf433fd
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-08-28 14:11:27 -0500

    codigo bien bonito y en orden

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 function simulador(){
 
 
+
+    const contPre = document.getElementById('pre-simulation');
+    const contPost = document.getElementById('post-simulation');
+
     //variables que campturas los valores de los campos
+    
     let cFullname = document.getElementById('fullname').value;
     let cTelefono = document.getElementById('telefono').value;
     let cEmail = document.getElementById('email').value;
@@ -25,50 +30,43 @@ function simulador(){
     }
 
     else{
-
-    switch (cTiempo) {
+        // hacemos el truco para quitar el desabled a una y ponerselo a otra
+        contPost.classList.remove('disabled');
+        contPre.classList.add('disabled');
+        switch (cTiempo) {
         //en este case realizaremos el calculo si el tiempo corresponde a un año
-        case "1":
-            ganancia = (cInversion * 0.008)*12;
-            total = parseInt(cInversion) + parseInt(ganancia);
-            sInteres.textContent = '0.08%';
-            sTiempo.textContent = '12 MESES';
-        break;
+            case "1":
+                ganancia = (cInversion * 0.008)*12;
+                total = parseInt(cInversion) + parseInt(ganancia);
+                sInteres.textContent = '0.08%';
+                sTiempo.textContent = '12 MESES';
+            break;
 
         //en este case realizaremos el calculo si el tiempo corresponde a dos años
-        case "2":
-            ganancia = (cInversion * 0.013)*24
-            total = parseInt(cInversion) + parseInt(ganancia);
-            sInteres.textContent = '1.13%';
-            sTiempo.textContent = '24 MESES';
-        break;
+            case "2":
+                ganancia = (cInversion * 0.013)*24
+                total = parseInt(cInversion) + parseInt(ganancia);
+                sInteres.textContent = '1.13%';
+                sTiempo.textContent = '24 MESES';
+            break;
 
         //en este case realizaremos el calculo si el tiempo corresponde a tres años
-        case "3":
-            ganancia = (cInversion * 0.017)*36
-            total = parseInt(cInversion) + parseInt(ganancia);
-            sInteres.textContent = '1.17%';
-            sTiempo.textContent = '36 MESES';            
-        break;
-    }
-}
+            case "3":
+                ganancia = (cInversion * 0.017)*36
+                total = parseInt(cInversion) + parseInt(ganancia);
+                sInteres.textContent = '1.17%';
+                sTiempo.textContent = '36 MESES';            
+            break;
+        }
 
-    
     sTotal.textContent= total;
     sGanacia.textContent= ganancia;
     sNombres.textContent= cFullname;
     sEmail.textContent= cEmail;
 
 
-    // hacemos el truco para quitar el desabled a una y ponerselo a otra
-    const contPre = document.getElementById('pre-simulation');
-    const contPost = document.getElementById('post-simulation');
-
-    contPost.classList.remove('disabled');
-    contPre.classList.add('disabled');
+    }
 }
-
-
 
 
 document.addEventListener('DOMContentLoaded',function(){
